86c8c3250c3877df35263f41410d8c6bf41aabd4
Andrey <arakelyants.andrey@yandex.ru>
2018-04-16 12:47:43 +0300

    Complete script, improve documentation.

--- README.md
+++ README.md
@@ -1,6 +1,19 @@
 # Frequency Analysis of Words
 
-[TODO. There will be project description]
+This script returns the list of 10 most frequent words in user`s text.
+
+# Quickstart
+
+The script requires Python v3.5. A path of file containing a text is the positional argument of the script.
+
+To run script on Linux:
+```
+$ python lang_frequency.py Edgar_Alan_Poe-Raven.txt
+Most frequent words in descending order:
+the, and, i, my, of, that, this, a, door, nevermore.
+```
+
+Windows usage is the same.
 
 # Project Goals
 
--- lang_frequency.py
+++ lang_frequency.py
@@ -1,39 +1,58 @@
 import argparse
 import re
 
+
+def get_console_arguments():
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        'file_path',
+        help='path to a text file'
+    )
+    args = parser.parse_args()
+    return args
+
+
 def load_data(file_path):
     with open(file_path, 'r', encoding='utf-8') as text_file:
         file_content = text_file.read()
     return file_content
 
 
-def get_most_frequent_words(text):
+def get_all_words_frequency(text):
     lower_text = text.lower()
-    words_list = re.findall(r'\b\w{1,}\b', lower_text)
-    words_dict = {}
+    words_list = re.findall(r'\b\w+\b', lower_text)
+    all_words_frequency = {}
     for word in words_list:
-        if word in words_dict.keys():
-            words_dict[word] += 1
+        if word in all_words_frequency.keys():
+            all_words_frequency[word] += 1
             continue
-        words_dict[word] = 1
-    return words_dict
+        all_words_frequency[word] = 1
+    return all_words_frequency
 
 
-def get_console_arguments():
-    parser = argparse.ArgumentParser()
-    parser.add_argument(
-        'file_path',
-        help='путь к файлу с текстом'
+def get_words_sorted_by_frequency(words_dict):
+    frequent_words_list = list(words_dict.keys())
+    frequent_words_list.sort(
+        key=lambda x: words_dict[x],
+        reverse=True
     )
-    args = parser.parse_args()
-    return args
+    return frequent_words_list
+
+
+def print_most_frequent_words(frequent_words, print_length):
+    print('Most frequent words in descending order:')
+    words_for_print = ', '.join(frequent_words[:print_length])
+    print(words_for_print + '.')
+
 
 if __name__ == '__main__':
+    WORDS_FOR_PRINT_NUMBER = 10
     console_arguments = get_console_arguments()
     user_file_path = console_arguments.file_path
     try:
         file_text = load_data(user_file_path)
     except FileNotFoundError:
-        exit('Не удалось найти файл с текстом.')
-    most_frequent_word = get_most_frequent_words(file_text)
-    print(most_frequent_word)
+        exit('Can not find the text file.')
+    all_words = get_all_words_frequency(file_text)
+    sorted_words = get_words_sorted_by_frequency(all_words)
+    print_most_frequent_words(sorted_words, WORDS_FOR_PRINT_NUMBER)
